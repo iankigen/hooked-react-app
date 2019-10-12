ce00a4163265fcf4d01a0eccf7b5b2e7dfd18a57
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-10-12 13:55:36 +0300

    chore: add Search Component

--- src/component/App.js
+++ src/component/App.js
@@ -78,6 +78,7 @@ const App = () => {
   return (
     <div className="App">
       <Header text={'HOOKED'}/>
+      <Search search={search}/>
       <p className='App-intro'>Sharing a few of our favourite movies</p>
       <div className='movies'>
         {loading && !errorMessage ? (
